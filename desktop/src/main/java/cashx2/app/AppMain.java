ce81c3e193d600fd01816b472f001d87a8509963
Emilio Wong <emilews.ew@gmail.com>
2019-08-06 16:01:36 -0700

    Revamped init on AppMain

--- desktop/src/main/java/cashx2/app/AppMain.java
+++ desktop/src/main/java/cashx2/app/AppMain.java
@@ -3,7 +3,6 @@ package cashx2.app;
 
 import cashx2.common.configurations.AppConfiguration;
 import cashx2.common.configurations.Version;
-import cashx2.core.bch.BchWallet;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -14,6 +13,8 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -23,23 +24,18 @@ public class AppMain extends Application {
     private final Version version = Version.getInstance();
     private final int width = appConfiguration.getWidth();
     private final int height = appConfiguration.getHeight();
-    //Wallet
-    public static BchWallet wallet;
-    //Menu booleans
-    private static boolean isInBCash = true;
-    private static boolean isInSlp = false;
 
 
     //For storing purposes
     private final File appDataPath = new File(appConfiguration.getAppdata());
     private final File seedDataPath = new File(appConfiguration.getSeedFileDirectory());
-
-
-    //For testing purposes
-    private static String address;
+    private final File store = new File(String.valueOf(seedDataPath));
 
     //List View
     ListView<String> addressesView = new ListView<String>();
+
+    //When there's no seed file, then it's time to create a new one
+    private boolean firstTime = false;
 
 
     //Menu bars and views
@@ -60,15 +56,14 @@ public class AppMain extends Application {
     public  void init() throws Exception{
         //Start reading from memory
         if(!appDataPath.exists()){
-            File store = new File(String.valueOf(seedDataPath));
             appDataPath.mkdirs();
-            if(store.createNewFile()){
-                System.out.println("Created new store!");
-            }
+        }
+        if(!store.exists()){
+            firstTime = true;
         }
         addressesView.setPrefWidth(500);
         addressesView.setPrefHeight(400);
-        
+
         //We add the menu and construct the panes
         menuBar.getMenus().addAll(Home, Settings);
         addressPane.setPadding(new Insets(20));
@@ -89,6 +84,21 @@ public class AppMain extends Application {
         primaryStage.setTitle(version.getAppName());
         primaryStage.setResizable(false);
         primaryStage.show();
+        if(firstTime){
+            Stage popup = new Stage();
+            BorderPane borderPane = new BorderPane();
+            VBox dialogVbox = new VBox(20);
+            dialogVbox.getChildren().add(new Text("Creating a new seed"));
+            borderPane.setCenter(dialogVbox);
+            Scene popupScene = new Scene(borderPane, 600,600);
+
+            popup.getIcons().add(icon);
+            popup.setScene(popupScene);
+            popup.setTitle("New seed");
+            popup.initModality(Modality.APPLICATION_MODAL);
+            popup.initOwner(primaryStage);
+            popup.show();
+        }
     }
 
     public static void main(String[] args) {
