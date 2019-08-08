4a6f0bb783bc349f25d256e50704cc6313ca45a2
Emilio Wong <emilews.ew@gmail.com>
2019-08-08 15:43:15 -0700

    Can display an unfinished first time popup window where a new seed is generated and displayed as well as input for a passphrase

--- core/src/main/java/cashx2/core/bch/AddressTest.java
+++ core/src/main/java/cashx2/core/bch/AddressTest.java
@@ -1,20 +1,9 @@
 package cashx2.core.bch;
 
-import org.bitcoinj.crypto.MnemonicCode;
-
 import java.io.IOException;
-import java.util.List;
 
 public class AddressTest {
 
     public static void main(String[] args) throws IOException {
-        MnemonicCode m = new MnemonicCode();
-        List<String> w = m.getWordList();
-        List<String> mnemo = null;
-        int rand = 0;
-        for(int i = 0; i <= 20; i++){
-            rand = (int)Math.random();
-            mnemo.add(w.get(rand));
-        }
     }
 }
--- desktop/src/main/java/cashx2/app/AppMain.java
+++ desktop/src/main/java/cashx2/app/AppMain.java
@@ -3,12 +3,15 @@ package cashx2.app;
 
 import cashx2.common.configurations.AppConfiguration;
 import cashx2.common.configurations.Version;
+import cashx2.common.reader.Reader;
+import cashx2.common.writer.Writer;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
@@ -16,8 +19,12 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.bitcoinj.crypto.MnemonicCode;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AppMain extends Application {
     private static AppConfiguration appConfiguration = AppConfiguration.getInstance();
@@ -31,8 +38,42 @@ public class AppMain extends Application {
     private final File seedDataPath = new File(appConfiguration.getSeedFileDirectory());
     private final File store = new File(String.valueOf(seedDataPath));
 
+    //For first time boot
+    private MnemonicCode mnemonicCode = null;
+    {
+        try {
+            mnemonicCode = new MnemonicCode();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private List<String> fullWordList = null;
+    private List<String> wordList = new ArrayList<String>();
+    private Writer writer = null;
+    private Reader reader = null;
+
+
     //List View
-    ListView<String> addressesView = new ListView<String>();
+    private ListView<String> addressesView = new ListView<String>();
+    //Seed view
+    private TextArea seedArea = new TextArea();{
+        seedArea.setEditable(false);
+        seedArea.setPrefWidth(300);
+    }
+    //Passphrase view from user
+    private TextArea passphrase1 = new TextArea();{
+        passphrase1.setPrefColumnCount(20);
+        passphrase1.setPrefRowCount(20);
+        passphrase1.setPromptText("Type passphrase...");
+    }
+    private TextArea passphrase2 = new TextArea();{
+        passphrase2.setPrefColumnCount(20);
+        passphrase2.setPrefRowCount(20);
+        passphrase2.setPromptText("Retype passphrase...");
+    }
+
+    //Button for first time
+
 
     //When there's no seed file, then it's time to create a new one
     private boolean firstTime = false;
@@ -59,8 +100,31 @@ public class AppMain extends Application {
             appDataPath.mkdirs();
         }
         if(!store.exists()){
+            fullWordList = mnemonicCode.getWordList();
+            int rand = 0;
+            for(int i = 0; i <= 20; i++){
+                rand = (int)(Math.random()*2048);
+                String s = null;
+                if (rand == 0){
+                    s = fullWordList.get(rand);
+                }else{
+                    s = fullWordList.get(rand-1);
+                }
+                wordList.add(s);
+            }
             firstTime = true;
         }
+
+
+        for(int i = 0; i < wordList.size(); i++){
+            if(i == wordList.size()-1){
+                seedArea.appendText(wordList.get(i));
+            }else{
+                seedArea.appendText(wordList.get(i) + " ");
+            }
+
+        }
+
         addressesView.setPrefWidth(500);
         addressesView.setPrefHeight(400);
 
@@ -89,8 +153,15 @@ public class AppMain extends Application {
             BorderPane borderPane = new BorderPane();
             VBox dialogVbox = new VBox(20);
             dialogVbox.getChildren().add(new Text("Creating a new seed"));
-            borderPane.setCenter(dialogVbox);
-            Scene popupScene = new Scene(borderPane, 600,600);
+            borderPane.setTop(dialogVbox);
+            borderPane.setCenter(seedArea);
+            BorderPane passPane = new BorderPane();
+            passPane.setPrefWidth(50);
+            passPane.setCenter(passphrase1);
+            passPane.setBottom(passphrase2);
+            passPane.setPrefHeight(40);
+            borderPane.setBottom(passPane);
+            Scene popupScene = new Scene(borderPane, 800,600);
 
             popup.getIcons().add(icon);
             popup.setScene(popupScene);
@@ -99,6 +170,14 @@ public class AppMain extends Application {
             popup.initOwner(primaryStage);
             popup.show();
         }
+    }
+
+    public void createDeterministicSeed(){
+
+    }
+    public void saveSeed(){
+
+
     }
 
     public static void main(String[] args) {
